cf5470bc992214d01fddd014c6774f269d7e6c5f
ferCasasDev <hfernandocasas@gmail.com>
2026-05-02 17:12:23 -0300

    Creacion Clase AccesoDatos

---
+++ TP_Catalogo_Articulos/Datos/AccesoDatos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class AccesoDatos
+    {
+
+        // declaracion de variables 
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        // propiedad solo lectura de lector 
+        public SqlDataReader Lector
+        {
+            get {  return lector; }
+        }
+
+        // constructor de la clase
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("sever=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        // método para setear la consulta 
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        // método para ejecutar la lectura 
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // método para cerrar conexión y lector
+        public void CerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+    }
+}
