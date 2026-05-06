b39e95f80ea5322d24a2de923824fd24ef8c7ec8
ferCasasDev <hfernandocasas@gmail.com>
2026-05-06 16:03:08 -0300

    Creacion metodo Listar

--- TP_Catalogo_Articulos/Dominio/Articulo.cs
+++ TP_Catalogo_Articulos/Dominio/Articulo.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Articulo
+    public class Articulo
     {
         public int Id { get; set; }
-        public int Numero { get; set; }
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
     }
---
+++ TP_Catalogo_Articulos/Negocio/ArticuloNegocio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Dominio;
+
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+
+        public List<Articulo> Listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+
+                datos.SetearConsulta("select Id, Codigo, Nombre, Descripcion from ARTICULOS");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+    }
+}
